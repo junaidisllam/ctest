b140932ef5a613e71dcc08decd008c62431607d0
junaidisllam <125545332+junaidisllam@users.noreply.github.com>
2023-05-25 18:01:23 +0600

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,141 @@
+		<!-- Topbar -->
+		<div class="topbar">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-5 col-md-12 col-12">
+						<!-- Top Left -->
+						<div class="top-left">
+							<ul class="list-main">
+								<li style="margin-top: -12px;"><i class="ti-headphone-alt hhh"></i><a class="aaa" href="tel:01710 809 132"> +880 1710 809 132 </a>, <a class="aaa" href="tel:01810 224 624">+880 1810 224 624</a>
+
+                                  
+
+
+
+                                        <div class="sinlge-bar shopping">
+								<a href="#" class="single-icon">
+									<div class="circle rl" style="margin-left: 14px;"><?php echo $firstLetter; ?></div></a>
+									<!-- Shopping Item -->
+								<div class="shopping-item">
+									<ul class="shopping-list">
+									   <li style="background: #fff;"><a href="logout.php"><i class="bi bi-box-arrow-right"></i>logout</a></li>
+									</ul>
+
+									</div>
+								</div>
+
+
+
+
+
+								</li>
+
+
+
+
+
+
+								<!-- <li><i class="ti-email"></i> ama1321256@gmail.com</li> -->
+							</ul>
+						</div>
+						<!--/ End Top Left -->
+					</div>
+					<div class="col-lg-7 col-md-12 col-12">
+						<!-- Top Right -->
+						<div class="right-content">
+							<ul class="list-main nnnnnnn">
+								<li><i class="ti-location-pin"></i> Store location</li>
+								<li><i class="ti-alarm-clock"></i> <a href="#">Daily deal</a></li>
+								<li><i class="ti-user"></i> <a href="#">My account</a></li>
+								<li>	<a href="#" class="single-icon">    <div class="circle"><?php echo $firstLetter; ?></div></a>
+</li>
+							</ul>
+						</div>
+						<!-- End Top Right -->
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- End Topbar -->
+		<div class="middle-inner">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-2 col-md-2 col-12">
+						<!-- Logo -->
+						<div class="logo">
+							<a href="inde.php"><img src="images/logo.png" alt="logo" class="logo_image"></a>
+						</div>
+						<!--/ End Logo -->
+						<!-- Search Form -->
+						<div class="search-top">
+							<div class="top-search"><a href="#0"><i class="ti-search"></i></a></div>
+							<!-- Search Form -->
+							<div class="search-top">
+								<form class="search-form">
+									<input type="text" placeholder="এখানে পণ্য অনুসন্ধান করুন....." name="search">
+									<button value="search" type="submit"><i class="ti-search"></i></button>
+								</form>
+							</div>
+							<!--/ End Search Form -->
+						</div>
+						<!--/ End Search Form -->
+						<div class="mobile-nav"></div>
+					</div>
+					<div class="col-lg-8 col-md-7 col-12">
+						<div class="search-bar-top">
+							<div class="search-bar">
+								<form>
+									<input name="search" placeholder="এখানে পণ্য অনুসন্ধান করুন....." type="search">
+									<button class="btnn"><i class="ti-search"></i></button>
+																<div class="sinlge-bar">
+							</div>
+								</form>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-2 col-md-3 col-12">
+						<div class="right-bar">
+							<!-- Search Form -->
+								<div class="sinlge-bar shopping">
+								<a href="#" class="single-icon"> <div class="circle"><?php echo $firstLetter; ?></div></a>
+								<!-- Shopping Item -->
+								<div class="shopping-item">
+									<ul class="shopping-list">
+									   <li><a href="logout.php"><i class="bi bi-box-arrow-right"></i>logout</a></li>
+									</ul>
+
+									</div>
+								</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Header Inner -->
+		<div class="header-inner">
+			<div class="container">
+				<div class="cat-nav-head">
+						<div class="col-lg-9 col-12">
+							<div class="menu-area">
+								<!-- Main Menu -->
+								<nav class="navbar navbar-expand-lg">
+									<div class="navbar-collapse">	
+										<div class="nav-inner">	
+											<ul class="nav main-menu menu navbar-nav">
+													<li class="active"><a href="#">হোম</a></li>	
+													<li><a href="#">সার্ভিস</a></li>			
+													<li><a href="contact.html">যোগাযোগ করুন</a></li>
+												</ul>
+										</div>
+									</div>
+								</nav>
+								<!--/ End Main Menu -->	
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--/ End Header Inner -->
+	<!--/ End Header -->
+	
