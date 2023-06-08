69c58da113eb60ffec63b693530e6a46d254864f
junaidisllam <125545332+junaidisllam@users.noreply.github.com>
2023-06-08 16:10:17 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,51 @@
-// Wait for the document to be ready
-$(document).ready(function() {
-  // Get the search input element
-  var searchInput = $('.search-input');
+  // Get the search input field and add an event listener
+  const searchInput = document.querySelector('.search-input');
+  searchInput.addEventListener('input', handleInput);
 
-  // Set up autocomplete
-  searchInput.autocomplete({
-    source: function(request, response) {
-      // Here, you can make an AJAX request to fetch autocomplete suggestions based on the user's input
-      // Replace the below example with your own implementation
-      var suggestions = [
-        'Apple',
-        'Banana',
-        'Orange',
-        'Pineapple',
-        'Strawberry'
-      ];
-
-      // Filter suggestions based on the user's input
-      var filteredSuggestions = $.grep(suggestions, function(item) {
-        return item.toLowerCase().indexOf(request.term.toLowerCase()) === 0;
+  // Function to handle user input
+  function handleInput() {
+    const userInput = searchInput.value;
+    
+    // Fetch search suggestions or use predefined array
+    const searchSuggestions = [
+      'apple',
+      'banana',
+      'cherry',
+      'date',
+      'elderberry',
+      'fig',
+      'grape',
+      'honeydew',
+      'kiwi',
+      'lemon',
+      'mango'
+    ];
+    
+    // Filter suggestions based on user input
+    const filteredSuggestions = searchSuggestions.filter(suggestion => {
+      return suggestion.toLowerCase().includes(userInput.toLowerCase());
+    });
+    
+    // Display the filtered suggestions in a dropdown menu
+    const dropdownMenu = document.createElement('ul');
+    dropdownMenu.classList.add('autocomplete-dropdown');
+    
+    filteredSuggestions.forEach(suggestion => {
+      const listItem = document.createElement('li');
+      listItem.textContent = suggestion;
+      listItem.addEventListener('click', () => {
+        searchInput.value = suggestion;
+        dropdownMenu.remove();
       });
-
-      // Provide the filtered suggestions to the autocomplete widget
-      response(filteredSuggestions);
+      dropdownMenu.appendChild(listItem);
+    });
+    
+    // Remove any existing dropdown menus
+    const existingDropdown = document.querySelector('.autocomplete-dropdown');
+    if (existingDropdown) {
+      existingDropdown.remove();
     }
-  });
-});
+    
+    // Append the dropdown menu below the search input field
+    searchInput.parentNode.appendChild(dropdownMenu);
+  }
